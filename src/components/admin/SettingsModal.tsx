1856a69454617935a3c54c81eae06ad4020a6d25
Ken0224JP <ken0224jp@gmail.com>
2026-02-05 22:17:34 +0900

    settings update

--- src/components/admin/SettingsModal.tsx
+++ src/components/admin/SettingsModal.tsx
@@ -34,7 +34,6 @@ const ToggleSwitch = ({
 }) => {
   return (
     <div className="flex items-center justify-between py-2 cursor-pointer" onClick={onChange}>
-      <span className="text-gray-700">{label}</span>
       <div className="relative inline-flex items-center cursor-pointer">
         <input 
           type="checkbox" 
@@ -43,7 +42,8 @@ const ToggleSwitch = ({
           readOnly // onChangeは親divで発火させるためreadOnlyにしています
         />
         {/* スイッチの背景 (OFF: グレー / ON: 青) */}
-        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-500"></div>
+      <span className="pl-2 text-gray-700">{label}</span>
       </div>
     </div>
   );
